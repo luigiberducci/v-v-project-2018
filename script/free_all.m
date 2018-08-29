f56f094bbbc6a5e40e13ba5607774533b9c3882b
luigiberducci <luigi.berd@gmail.comqqclear>
2018-08-29 11:20:04 +0200

    added comments and code clear

--- script/free_all.m
+++ script/free_all.m
@@ -1,2 +1,6 @@
-close_system(MODEL_NAME);  %Close the model
-clear;                  %Clear the workspace
+% free_all(): void
+% PRE:  MODEL_NAME already defined in the workspace
+% POST: MODEL_NAME closed, MATLAB workspace empty
+
+close_system(MODEL_NAME);   %Close the model
+clear;                      %Clear the workspace
--- script/new_trace.m
+++ script/new_trace.m
@@ -1,3 +1,7 @@
+% new_trace(): void
+% PRE:  MODEL_NAME, BLOCK, GROUP variables already defined in the workspace
+% POST: disturbance trace (model's input as throttle,brake signals) updated randomly
+
 %Signal parameters
 TIME_MIN        = 0;    %Time period [TIME_MIN, TIME_MAX]
 TIME_MAX        = 150;  
@@ -10,21 +14,19 @@ BRAKE_MAX       = 5650;
 
 %Create Throttle and Brake domains
 SIZE_DOM        = 10;
-BRAKE_STEP = floor( (BRAKE_MAX-BRAKE_MIN) / (SIZE_DOM-1) ); %The minus one is because we consider 0 value in domain
-BRAKE_DOM  = BRAKE_MIN:BRAKE_STEP:BRAKE_MAX;
-THROTTLE_STEP = floor( (THROTTLE_MAX-THROTTLE_MIN)/ (SIZE_DOM-1) );
-THROTTLE_DOM  = THROTTLE_MIN:THROTTLE_STEP:THROTTLE_MAX;
+BRAKE_STEP      = floor( (BRAKE_MAX-BRAKE_MIN) / (SIZE_DOM-1) ); %The minus one is because we consider 0 value in domain
+BRAKE_DOM       = BRAKE_MIN:BRAKE_STEP:BRAKE_MAX;
+THROTTLE_STEP   = floor( (THROTTLE_MAX-THROTTLE_MIN)/ (SIZE_DOM-1) );
+THROTTLE_DOM    = THROTTLE_MIN:THROTTLE_STEP:THROTTLE_MAX;
 
 %Create signals
 n  = NUM_TICKS;  %Number of values to generate
-%DT = THROTTLE_MIN + rand(1,n) * (THROTTLE_MAX-THROTTLE_MIN);    %Generate random values for Throttle signal
-%DB = BRAKE_MIN    + rand(1,n) * (BRAKE_MAX-BRAKE_MIN);          %Generate random values for Brake signal
 DT = ceil( 0 + rand(1,n) * SIZE_DOM );
 DB = ceil( 0 + rand(1,n) * SIZE_DOM );
 
-TIME_VECTOR = TIME_MIN : TIME_STEP : TIME_MAX;   %Create time vector 
-D_THROTTLE  = [ 0 THROTTLE_DOM(DT) ];     %Create Throttle vector, it starts from 0 because the vehicle is stopped
-D_BRAKE     = [ 0 BRAKE_DOM(DB) ];     %Create Brake vector, it starts from 0 because the vehicle is stopped
+TIME_VECTOR = TIME_MIN : TIME_STEP : TIME_MAX;      %Create time vector 
+D_THROTTLE  = [ 0 THROTTLE_DOM(DT) ];               %Create Throttle vector, it starts from 0 because the vehicle is stopped
+D_BRAKE     = [ 0 BRAKE_DOM(DB) ];                  %Create Brake vector, it starts from 0 because the vehicle is stopped
 
 %Set signals in the model
 signalbuilder(BLOCK, 'SET', 'Throttle', GROUP, TIME_VECTOR, D_THROTTLE);
